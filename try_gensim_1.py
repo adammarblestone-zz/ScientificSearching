fc3a06802e571b43f37d25ee456c4fa87cfca3ca
Adam <adam.h.marblestone@gmail.com>
2014-06-09 13:30:23 -0400

    looks like it made the corpus file sucessfully

--- try_gensim_1.py
+++ try_gensim_1.py
@@ -1,23 +1,38 @@
-from gensim import corpora, models, similarities
+import gensim
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import io
 
 def main():
     indir = "neuroscience_abstracts"
-    c = abstractsCorpus(indir)
-    for vector in c:
-        print vector
     
+    # Construct the corpus
+    c = abstractsCorpus(indir)
+    
+    # Save the corpus
+    print "Saving the corpus."
+    gensim.corpora.MmCorpus.serialize('abstracts_corpus_1.mm', c)
+
+def iter_documents(indir):
+    print "Loading tokens into dictionary..."
+    for filename in os.listdir(indir):
+        if filename[:9] == "abstracts":
+            print "Filename: %s" % filename
+            for line in open(indir + "/" + filename).readlines():
+                if len(line) > 1:
+                    yield gensim.utils.tokenize(line.decode('ascii', 'ignore').strip(), lower=True)
     
 class abstractsCorpus(object):
     def __init__(self, indir):
         self.indir = indir
-    
+        self.dictionary = gensim.corpora.Dictionary(iter_documents(indir))
+        self.dictionary.filter_extremes(no_below = 1, keep_n = 30000) # check API docs for pruning params    
+        
     def __iter__(self):
-        for filename in os.listdir(self.indir):
-            for line in open(filename):
-                yield dictionary.doc2bow(line.lower().split())
+        print "Creating vector corpus from tokens..."
+        for tokens in iter_documents(self.indir):
+            yield self.dictionary.doc2bow(tokens)
     
 if __name__ == '__main__':
     main()

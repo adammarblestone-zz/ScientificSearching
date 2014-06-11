19cc2f333916b714fab5f5efb7fe18b8799c97ba
Adam <adam.h.marblestone@gmail.com>
2014-06-11 17:06:12 -0400

    words corresponding to difference vectors didn't work that well, but illustrated how to do some operations

---
+++ test_word2vec_2.py
@@ -0,0 +1,33 @@
+import gensim
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import io
+import nltk
+import logging
+
+#logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+def main():
+    print "\nLoading Word2Vec model...\n"
+    model = gensim.models.Word2Vec.load("word2vec_model_1")
+    model.init_sims(replace=True)
+        
+    for s in ["glia"]:
+        print "\nMost similar words to: %s \n" % s
+        print model.most_similar(positive=[s])
+        print "\n"
+        
+        # get difference vectors, and find the word most similar to each difference vector: this did not give "meaningful" results
+        sims = model.most_similar(positive=[s])
+        for k in sims:
+            print "\nrelated word:"
+            print k[0]
+            diff = model[s] - model[k[0]]
+            print "most similar vector to difference vector:"
+            dot_products = [np.dot(model[model.index2word[i]], diff) for i in range(len(model.index2word))]
+            closest_word = model.index2word[dot_products.index(max(dot_products))]
+            print closest_word
+
+if __name__ == '__main__':
+    main()

d4aa56a3e4c9a2fd4ec04bb09f2d6f3cb37327e4
Adam Marblestone <adam.h.marblestone@gmail.com>
2014-07-18 18:08:35 -0400

    added some ngram stuff

--- ExploreBioNLPData/ngrams_over_time_1_parallelized.py
+++ ExploreBioNLPData/ngrams_over_time_1_parallelized.py
@@ -3,15 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-input_string = "fMRI"
+input_string = "intravascular electrode"
 timerange = range(1990,2014)
 counts = [0.0 for i in timerange]
 input_word_list = input_string.split()
 n = len(input_word_list)
 
 def count_ngram(year):
-	print "Searching in year %i ..." % year
-
 	filename_base = "../../PubMed/BioNLP/ngrams/pubmed/" + str(n) + "-grams-"
 	filename = filename_base + str(year) + ".tsv.gz"
 	
@@ -20,13 +18,15 @@ def count_ngram(year):
     	for line in lines:
 		elements = line.rstrip().split("\t")
 		if elements[0] == input_string:
+			print elements
 			return [year, int(elements[2])]
 
 	# otherwise
 	return [year,0.0]
 	
 def main():
-	
+	print "Searching for ngrams..."
+
 	# simple parallelization over the year-specific input files
 	from multiprocessing import Pool
 	pool = Pool(processes = 6)

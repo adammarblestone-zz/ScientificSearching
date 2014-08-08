d9d7798ca6387f591c2ebbc3d8c3ab2e5a660981
Adam <adam.h.marblestone@gmail.com>
2014-08-08 15:15:30 -0400

    added ontology parsing

---
+++ ExploreNIF/print_ontologies.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import os
+
+def main():
+    
+    named_entities = []
+    
+    indir = "../NIF_OWL_files"
+    for filename in os.listdir(indir):
+        f = open(indir + "/" + filename, 'r')
+        soup = BeautifulSoup(f)
+        for link in soup.find_all("rdfs:label"):
+            named_entities.append(link.contents[0])
+        f.close()
+    
+    outfile = open("../../named_entities.txt", 'w')
+    for n in sorted(named_entities):
+        print str(n)
+        outfile.write(n + "\n")
+    outfile.close()
+        
+        
+if __name__ == '__main__':
+    main()

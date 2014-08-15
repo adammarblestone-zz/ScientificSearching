af133d9ddbc1ffa6ea9d46ef7ee91ec7eab4772f
Adam Marblestone <adam.h.marblestone@gmail.com>
2014-08-15 14:22:17 -0400

    added test for cleaned phrase model

---
+++ ExplorePMC/test_PMC_phrase_vectors_similar_words_to_query_cleaned_model_01.py
@@ -0,0 +1,40 @@
+import gensim
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import io
+import nltk
+import logging
+
+#logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+'''Displays similar terms to an input term.'''
+
+def main():
+    print "\nLoading Word2Vec model...\n"
+    # 4 GB input file, uses about 20 GB of memory when loaded
+    model = gensim.models.Word2Vec.load("../../PubMed/NEURO-PHRASE-VECTOR-CLEANED-NONBINARY")
+    model.init_sims(replace = True)
+    vocab = model.index2word
+
+    import readline
+    readline.parse_and_bind("tab: complete")
+    def complete(text,state):
+        results = [x for x in vocab if x.startswith(text)] + [None]
+        return results[state]
+
+    readline.set_completer(complete)
+
+    while True:
+        line = raw_input('\nprompt> ')
+        if line in vocab:
+            print "\nSimilar words:"
+            print "______________"
+            sims = model.most_similar(positive=[line]) # this needs to be changed to allow phrases
+            for p in sims:
+                print p
+        else:
+            print "Sorry: not in vocabulary!"
+            
+if __name__ == '__main__':
+    main()
